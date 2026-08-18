3db5d6dffcd23fa3e903d504703fdce8aa64dce2
Akshaya-9865 <akshu.r20052005@gmail.com>
2026-08-18 11:45:19 +0530

    Add files via upload

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      backdropBlur: {
+        xs: "2px",
+      },
+    },
+  },
+  plugins: [],
+};
+
